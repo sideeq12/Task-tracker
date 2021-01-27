fbe35e50e5700c98c56c92adba48ac79c902f07b
sideeq12 <abdwaheed2018@gmail.com>
2021-01-27 22:38:12 +0100

    react version

--- src/components/task.js
+++ src/components/task.js
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const Tasks = ({tasks}) => {
+
+const Tasks = () => {
+    const [tasks, setTask]= useState([
+        {
+            id : 1,
+            reminder : true,
+            text : "Appointment with Doctor",
+            Date : "February 15th, 2021."
+        }, {
+            id : 2,
+            reminder : false,
+            text : "Project Meeting",
+            Date : "January 20th, 2021."
+        }, 
+        {
+            id : 3,
+            reminder : true,
+            text : "Food campaign",
+            Date : "February 11th, 2021."
+        }
+    ])
     return ( <div>
         {tasks.map((task)=>{
             return(<h2 key={task.id}>{task.text}</h2>)
--- src/components/wrappers/App.js
+++ src/components/wrappers/App.js
@@ -4,29 +4,10 @@ import Header from "../Header"
 import Tasks from '../Task';
 
 function App(){
-    const [tasks, setTasks] = useState([
-        {
-            id : 1,
-            reminder : true,
-            text : "Appointment with Doctor",
-            Day : "February 15th, 2021."
-        }, {
-            id : 2,
-            reminder : false,
-            text : "Project Meeting",
-            Day : "January 20th, 2021."
-        }, 
-        {
-            id : 3,
-            reminder : true,
-            text : "Food campaign",
-            Day : "February 11th, 2021."
-        }
-    
-    ])
+  
     return(<div className="container">
     <Header  />
-    <Tasks  tasks={tasks}/>
+    <Tasks />
     </div>)
 }
 

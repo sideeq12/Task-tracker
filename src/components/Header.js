59d677719b47845182e11df9f319006d061d8727
sideeq12 <abdwaheed2018@gmail.com>
2021-01-27 16:31:48 +0100

    header fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,28 @@
 import React from 'react'
+import Button from "./Button"
 
 const Header = ({title})=>{
+    const click = ()=>{
+        console.log("click")
+    }
     return(
-        <header>
-            <h1>{title}</h1>
+        <header style={style}>
+            <h2>{title}</h2>
+            <Button  color="#ef4f4f" text="Close" onClick={click}/>
         </header>
     )
 }
 
+const style ={
+    color: "white",
+    backgroundColor : "#0C4259",
+    padding : "10px",
+    textAlign : "center",
+    display : "flex",
+    flexDirection : "row",
+    justifyContent: "space-between",
+    alignItems : "center"
+}
 Header.defaultProps ={
     title : "Task Tracker"
 }

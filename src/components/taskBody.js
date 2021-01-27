6441624372eb440c8751e66dc419b67071c3ad86
sideeq12 <abdwaheed2018@gmail.com>
2021-01-27 16:41:06 +0100

    task array

---
+++ src/components/taskBody.js
@@ -0,0 +1,22 @@
+const tasks = [
+    {
+        id : 1,
+        reminder : true,
+        text : "Appointment with Doctor",
+        Date : "February 15th, 2021."
+    }, {
+        id : 2,
+        reminder : false,
+        text : "Project Meeting",
+        Date : "January 20th, 2021."
+    }, 
+    {
+        id : 3,
+        reminder : true,
+        text : "Food campaign",
+        Date : "February 11th, 2021."
+    },
+
+]
+
+export default  tasks;

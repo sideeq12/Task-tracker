ba9295d3b5413fd84d4ee86822ac67e164fec549
sideeq12 <abdwaheed2018@gmail.com>
2021-02-01 16:52:02 +0100

    events

--- src/components/upper-segment/input-components/input.js
+++ src/components/upper-segment/input-components/input.js
@@ -2,7 +2,7 @@ import "./input.css"
 import useState from "react"
 
 const Text = ({text}) => {
-    const [Text, setText] = useState("")
+    const [Text, setText] = useState(" ")
     return ( <div className="cover">
         <label for="text">{text}</label> <br/>
         <input type="text" className="form-control" name="text" placeholder={Text} onChange={(e)=>setText(e.target.value)}></input>   

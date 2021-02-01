4ab45c17b9bc55881e56c8170b8a29a78483e933
sideeq12 <abdwaheed2018@gmail.com>
2021-02-01 14:52:51 +0100

    icons

--- src/components/lower-segment/task-design.js
+++ src/components/lower-segment/task-design.js
@@ -1,9 +1,10 @@
 import "./task.css"
+import {FaTimes} from "react-icons/fa"
 
 const Design = ({text, day}) => {
     return (  <div>
         <div className="task">
-            <h4>{text}</h4>
+            <h4>{text} <FaTimes /></h4>
             <span>{day}</span>
         </div>
     </div>);

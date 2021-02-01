8dc48c9ea6cf490629fb8cba8c50ec8bb0de1f5f
sideeq12 <abdwaheed2018@gmail.com>
2021-02-01 14:59:55 +0100

    style task list

--- src/components/lower-segment/task-design.js
+++ src/components/lower-segment/task-design.js
@@ -4,7 +4,7 @@ import {FaTimes} from "react-icons/fa"
 const Design = ({text, day}) => {
     return (  <div>
         <div className="task">
-            <h4>{text} <FaTimes /></h4>
+            <h4>{text} <FaTimes  style={{marginLeft : "20px", color : "red", cursor : "pointer"}}/></h4>
             <span>{day}</span>
         </div>
     </div>);
